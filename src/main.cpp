b9bdfedc68c9f2064644360d1505e9a9139dda72
Borin Ouch <borin.ouch@gmail.com>
2026-03-18 20:55:20 -0700

    Create window, init GLAD, run update loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,47 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-auto main() -> int {
-    std::println("Hello, world!!");
 
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+void FrameBufferSizeCallback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+auto main() -> int {
+  std::println("Hello, world!!");
+
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #if defined(__APPLE__)
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    return 0;
+  GLFWwindow* window = glfwCreateWindow(800, 600, "Learning OpenGL AGAIN!", nullptr, nullptr);
+  if (window == nullptr) {
+    std::println(stderr, "Failed to create GLFW Window");
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::println(stderr, "Failed to initialize GLAD");
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
+
+  glfwSetFramebufferSizeCallback(window, FrameBufferSizeCallback);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
+  return 0;
 }
 

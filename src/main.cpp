0feee626e848ea0d4a5ccd3ec8abdc48987b00bc
Borin Ouch <borin.ouch@gmail.com>
2026-03-18 20:37:14 -0700

    Initialize glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <print>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 auto main() -> int {
     std::println("Hello, world!!");
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if defined(__APPLE__)
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
     return 0;
 }
 

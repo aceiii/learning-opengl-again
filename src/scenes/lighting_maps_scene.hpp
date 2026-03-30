643e0d8af54339c7b46b2466a06b5e9f2738a657
Borin Ouch <borin.ouch@gmail.com>
2026-03-29 22:29:37 -0700

    Remove built-in materials from last section

--- src/scenes/lighting_maps_scene.hpp
+++ src/scenes/lighting_maps_scene.hpp
@@ -103,7 +103,7 @@ public:
     lighting_shader_.SetVec3("light.ambient", light_.ambient);
     lighting_shader_.SetVec3("light.diffuse", light_.diffuse);
     lighting_shader_.SetVec3("light.specular", light_.specular);
-    lighting_shader_.SetInt("material.diffuse", 0);
+    lighting_shader_.SetInt("material.diffuse", material_.diffuse);
     lighting_shader_.SetVec3("material.specular", material_.specular);
     lighting_shader_.SetFloat("material.shininess", material_.shininess);
 
@@ -150,29 +150,9 @@ public:
       }
       if (ImGui::CollapsingHeader("Material", ImGuiTreeNodeFlags_DefaultOpen)) {
         ImGui::PushID("Material");
-        if (ImGui::BeginCombo("Selected material", selected_material_idx_ < 0 ? "Custom" : kBuiltinMaterials[selected_material_idx_].first)) {
-          for (int i = 0; i < kBuiltinMaterials.size(); i++) {
-            const auto& mat = kBuiltinMaterials[i];
-            if (ImGui::Selectable(mat.first, i == selected_material_idx_)) {
-              material_ = mat.second;
-              selected_material_idx_ = i;
-            }
-          }
-          ImGui::EndCombo();
-        }
-
-        if (ImGui::ColorEdit3("Ambient", &material_.ambient[0])) {
-          selected_material_idx_ = -1;
-        }
-        if (ImGui::ColorEdit3("Diffuse", &material_.diffuse[0])) {
-          selected_material_idx_ = -1;
-        }
-        if (ImGui::ColorEdit3("Specular", &material_.specular[0])) {
-          selected_material_idx_ = -1;
-        }
-        if (ImGui::DragFloat("Shininess", &material_.shininess, 0.01f, 0.01f, 10.0f)) {
-          selected_material_idx_ = -1;
-        }
+        ImGui::DragInt("Diffuse", &material_.diffuse, 1, 0, 8);
+        ImGui::ColorEdit3("Specular", &material_.specular[0]);
+        ImGui::DragFloat("Shininess", &material_.shininess, 0.01f, 0.01f, 10.0f);
         ImGui::PopID();
       }
       if (ImGui::CollapsingHeader("Camera")) {
@@ -288,8 +268,7 @@ private:
   }
 
   struct Material {
-    glm::vec3 ambient;
-    glm::vec3 diffuse;
+    int diffuse;
     glm::vec3 specular;
     float shininess;
   };
@@ -346,37 +325,6 @@ private:
     -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
   };
 
-  inline static const std::array kBuiltinMaterials{
-    std::make_pair("Emerald", Material{ .ambient = { 0.0215f, 0.1745f, 0.0215f }, .diffuse = { 0.07568f, 0.61424f, 0.07568f }, .specular = { 0.633f, 0.727811f, 0.633f }, .shininess = 76.8f }),
-    std::make_pair("Jade", Material{ .ambient = { 0.135f, 0.2225f, 0.1575f }, .diffuse = { 0.54f, 0.89f, 0.63f }, .specular = { 0.316228f, 0.316228f, 0.316228f }, .shininess = 12.8f }),
-    std::make_pair("Obsidian", Material{ .ambient = { 0.05375f, 0.05f, 0.06625f }, .diffuse = { 0.18275f, 0.17f, 0.22525f }, .specular = { 0.332741f, 0.328634f, 0.346435f }, .shininess = 38.4f }),
-
-    std::make_pair("Pearl", Material{ .ambient = { 0.25f, 0.20725f, 0.20725f }, .diffuse = { 1.0f, 0.829f, 0.829f }, .specular = { 0.296648f, 0.296648f, 0.296648f }, .shininess = 11.264f }),
-    std::make_pair("Ruby", Material{ .ambient = { 0.1745f, 0.01175f, 0.01175f }, .diffuse = { 0.61424f, 0.04136f, 0.04136f }, .specular = { 0.727811f, 0.626959f, 0.626959f }, .shininess = 76.8f }),
-    std::make_pair("Turquoise", Material{ .ambient = { 0.1f, 0.18725f, 0.1745f }, .diffuse = { 0.396f, 0.74151f, 0.69102f }, .specular = { 0.297254f, 0.30829f, 0.306678f }, .shininess = 12.8f }),
-
-    std::make_pair("Brass", Material{ .ambient = { 0.329412f, 0.223529f, 0.027451f }, .diffuse = { 0.780392f, 0.568627f, 0.113725f }, .specular = { 0.992157f, 0.941176f, 0.807843f }, .shininess = 27.8974f }),
-    std::make_pair("Bronze", Material{ .ambient = { 0.2125f, 0.1275f, 0.054f }, .diffuse = { 0.714f, 0.4284f, 0.18144f }, .specular = { 0.393548f, 0.271906f, 0.166721f }, .shininess = 25.6f }),
-    std::make_pair("Chrome", Material{ .ambient = { 0.25f, 0.25f, 0.25f }, .diffuse = { 0.4f, 0.4f, 0.4f }, .specular = { 0.774597f, 0.774597f, 0.774597f }, .shininess = 76.8f }),
-    std::make_pair("Copper", Material{ .ambient = { 0.19125f, 0.0735f, 0.0225f }, .diffuse = { 0.7038f, 0.27048f, 0.0828f }, .specular = { 0.256777f, 0.137622f, 0.086014f }, .shininess = 12.8f }),
-    std::make_pair("Gold", Material{ .ambient = { 0.24725f, 0.1995f, 0.0745f }, .diffuse = { 0.75164f, 0.60648f, 0.22648f }, .specular = { 0.628281f, 0.555802f, 0.366065f }, .shininess = 51.2f }),
-    std::make_pair("Silver", Material{ .ambient = { 0.19225f, 0.19225f, 0.19225f }, .diffuse = { 0.50754f, 0.50754f, 0.50754f }, .specular = { 0.508273f, 0.508273f, 0.508273f }, .shininess = 51.2f }),
-
-    std::make_pair("Black Plastic", Material{ .ambient = { 0.0f, 0.0f, 0.0f }, .diffuse = { 0.01f, 0.01f, 0.01f }, .specular = { 0.50f, 0.50f, 0.50f }, .shininess = 32.0f }),
-    std::make_pair("Cyan Plastic", Material{ .ambient = { 0.0f, 0.1f, 0.06f }, .diffuse = { 0.0f, 0.50980392f, 0.50980392f }, .specular = { 0.50196078f, 0.50196078f, 0.50196078f }, .shininess = 32.0f }),
-    std::make_pair("Green Plastic", Material{ .ambient = { 0.0f, 0.0f, 0.0f }, .diffuse = { 0.1f, 0.35f, 0.1f }, .specular = { 0.45f, 0.55f, 0.45f }, .shininess = 32.0f }),
-    std::make_pair("Red Plastic", Material{ .ambient = { 0.0f, 0.0f, 0.0f }, .diffuse = { 0.5f, 0.0f, 0.0f }, .specular = { 0.7f, 0.6f, 0.6f }, .shininess = 32.0f }),
-    std::make_pair("White Plastic", Material{ .ambient = { 0.0f, 0.0f, 0.0f }, .diffuse = { 0.55f, 0.55f, 0.55f }, .specular = { 0.70f, 0.70f, 0.70f }, .shininess = 32.0f }),
-    std::make_pair("Yellow Plastic", Material{ .ambient = { 0.0f, 0.0f, 0.0f }, .diffuse = { 0.5f, 0.5f, 0.0f }, .specular = { 0.60f, 0.60f, 0.50f }, .shininess = 32.0f }),
-
-    std::make_pair("Black Rubber", Material{ .ambient = { 0.02f, 0.02f, 0.02f }, .diffuse = { 0.01f, 0.01f, 0.01f }, .specular = { 0.4f, 0.4f, 0.4f }, .shininess = 10.0f }),
-    std::make_pair("Cyan Rubber", Material{ .ambient = { 0.0f, 0.05f, 0.05f }, .diffuse = { 0.4f, 0.5f, 0.5f }, .specular = { 0.04f, 0.7f, 0.7f }, .shininess = 10.0f }),
-    std::make_pair("Green Rubber", Material{ .ambient = { 0.0f, 0.05f, 0.0f }, .diffuse = { 0.4f, 0.5f, 0.4f }, .specular = { 0.04f, 0.7f, 0.04f }, .shininess = 10.0f }),
-    std::make_pair("Red Rubber", Material{ .ambient = { 0.05f, 0.0f, 0.0f }, .diffuse = { 0.5f, 0.4f, 0.4f }, .specular = { 0.7f, 0.04f, 0.04f }, .shininess = 10.0f }),
-    std::make_pair("White Rubber", Material{ .ambient = { 0.05f, 0.05f, 0.05f }, .diffuse = { 0.5f, 0.5f, 0.5f }, .specular = { 0.7f, 0.7f, 0.7f }, .shininess = 10.0f }),
-    std::make_pair("Yellow Rubber", Material{ .ambient = { 0.05f, 0.05f, 0.0f }, .diffuse = { 0.5f, 0.5f, 0.4f }, .specular = { 0.7f, 0.7f, 0.04f }, .shininess = 10.0f }),
-  };
-
   IAppContext* ctx_ = nullptr;
 
   inline static const float kDefaultYaw = -90.0f;
@@ -406,8 +354,7 @@ private:
 
   Camera camera_{glm::vec3(0.0f, 0.0f, 3.0f)};
   Material material_{
-    .ambient = glm::vec3(1.0f, 0.5f, 0.31f),
-    .diffuse = glm::vec3(1.0f, 0.5f, 0.31f),
+    .diffuse = 0,
     .specular = glm::vec3(0.5f, 0.5f, 0.5f),
     .shininess = 32.0f,
   };
@@ -421,6 +368,4 @@ private:
   glm::mat4 projection_;
 
   glm::vec2 last_mouse_;
-
-  int selected_material_idx_ = -1;
 };

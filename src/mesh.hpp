dba52f367bf61079159a5a9d0f35f46471711156
Borin Ouch <borin.ouch@gmail.com>
2026-04-03 10:59:24 -0700

    Add Mesh class

---
+++ src/mesh.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <glm/glm.hpp>
+
+#include "shader.hpp"
+
+
+struct Vertex {
+  glm::vec3 position;
+  glm::vec3 normal;
+  glm::vec3 tex_coords;
+};
+
+struct Texture {
+  unsigned int id;
+  std::string type;
+};
+
+class Mesh {
+public:
+  std::vector<Vertex> vertices;
+  std::vector<unsigned int> indices;
+  std::vector<Texture> textures;
+
+  Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+  void Draw(Shader& shader);
+
+private:
+  void SetupMesh();
+
+  unsigned int vao_, vbo_, ebo_;
+};

0518929bfafee1f6674f28cbbbbdbd0bc1428a3d
Borin Ouch <borin.ouch@gmail.com>
2026-05-16 22:14:29 -0700

    Implement pcf

--- resources/shaders/point_shadows_scene/main.fs
+++ resources/shaders/point_shadows_scene/main.fs
@@ -23,42 +23,36 @@ uniform bool enableShadows;
 uniform bool showDepth;
 
 float ShadowCalculation(vec3 fragPos, vec3 normal, vec3 lightDir) {
-    // float shadow = 0.0;
-    // vec3 projCoords = fragPos.xyz / fragPos.w;
-    // projCoords = projCoords * 0.5 + 0.5;
-
-    // float closestDepth = texture(shadowMap, projCoords.xy).r;
-    // float currentDepth = projCoords.z;
-
-    // float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
-
-    // if (usePCF) {
-    //     vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
-    //     for (int x = -1; x <= 1; ++x) {
-    //         for (int y = -1; y <= 1; ++y) {
-    //             float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r;
-    //             shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;
-    //         }
-    //     }
-    //     shadow /= 9.0;
-    // } else {
-    //     shadow = currentDepth - bias > closestDepth ? 1.0 : 0.0;
-    // }
-
-    // if (projCoords.z > 1.0) {
-    //     shadow = 0.0;
-    // }
-
     vec3 fragToLight = fragPos - lightPos;
-    float closestDepth = texture(depthMap, fragToLight).r;
-    closestDepth *= farPlane;
 
+    float shadow = 0.0;
     float currentDepth = length(fragToLight);
     float bias = 0.05;
-    float shadow = currentDepth - bias > closestDepth ? 1.0 : 0.0;
+    float samples = 4.0;
+    float offset = 0.1;
 
-    if (showDepth) {
-        FragColor = vec4(vec3(closestDepth / farPlane), 1.0);
+    if (usePCF && !showDepth) {
+        for (float x = -offset; x < offset; x+= offset / (samples * 0.5)) {
+            for (float y = -offset; y < offset; y += offset / (samples * 0.5)) {
+                for (float z = -offset; z < offset; z += offset / (samples * 0.5)) {
+                    float closestDepth = texture(depthMap, fragToLight + vec3(x, y, z)).r;
+                    closestDepth *= farPlane;
+                    if (currentDepth - bias > closestDepth) {
+                        shadow += 1.0;
+                    }
+                }
+            }
+        }
+
+        shadow /= (samples * samples * samples);
+    } else {
+        float closestDepth = texture(depthMap, fragToLight).r;
+        closestDepth *= farPlane;
+        shadow = currentDepth - bias > closestDepth ? 1.0 : 0.0;
+
+        if (showDepth) {
+            FragColor = vec4(vec3(closestDepth / farPlane), 1.0);
+        }
     }
 
     return shadow;

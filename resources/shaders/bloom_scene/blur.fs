b2ada52e5430e50032c47ef51b9b3640c315223d
Borin Ouch <borin.ouch@gmail.com>
2026-05-29 21:25:22 -0700

    Add back zero weight on blur

--- resources/shaders/bloom_scene/blur.fs
+++ resources/shaders/bloom_scene/blur.fs
@@ -11,7 +11,7 @@ uniform float weight[5] = float[] (0.2270270270, 0.1945945946, 0.1216216216, 0.0
 
 void main() {
     vec2 tex_offset = 1.0 / textureSize(image, 0);
-    vec3 result = texture(image, TexCoords).rgb;// * weight[0];
+    vec3 result = texture(image, TexCoords).rgb * weight[0];
     if (horizontal) {
         for (int i = 1; i < 5; i++) {
             result += texture(image, TexCoords + vec2(tex_offset.x *  i, 0.0)).rgb * weight[i];

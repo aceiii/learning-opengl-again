55de8c583dbcbd9038f1e9decd0a71db8a5dd9a3
Borin Ouch <borin.ouch@gmail.com>
2026-06-09 22:53:55 -0700

    Tweak sdf code for fun

--- resources/shaders/pbr_scene/main.fs
+++ resources/shaders/pbr_scene/main.fs
@@ -11,20 +11,35 @@ uniform mat4 projection;
 
 uniform vec3 viewPos;
 
-float sphere(vec2 p, vec2 c, float r) {
-    return clamp(length(p - c) - r, 0.0, 1.0);
+float sphere(vec2 p, float r) {
+    return length(p) - r;
 }
 
-float box(vec2 p, vec2 center, vec2 size) {
-    vec2 d = abs(p - center) - (size / 2.0);
-    return clamp(length(max(d, 0.0)) + min(max(d.x, d.y), 0.0), 0.0, 1.0);
+float box(vec2 p, vec2 size) {
+    vec2 d = abs(p) - (size / 2.0);
+    return length(max(d, 0.0)) + min(max(d.x, d.y), 0.0);
+}
+
+vec2 translate(vec2 a, vec2 b) {
+    return a - b;
+}
+
+vec2 rotate(vec2 p, float r) {
+    float s = sin(r);
+    float c = cos(r);
+    return vec2(c * p.x + s * p.y, c * p.y - s * p.x);
 }
 
 void main() {
     vec2 pos = vec2(TexCoords.x, -TexCoords.y + 1.0) * vec2(width, height);
 
-    vec3 result = vec3(sphere(pos, vec2(width / 2, height / 2), 100.0), 0.0, 0.0);
-    result *= vec3(box(pos, vec2(200, 200), vec2(250, 325)), 0.0, 0.0);
+    vec3 result = vec3(sphere(translate(pos, vec2(width / 2, height / 2)), 100.0), 0.0, 0.0);
+    result *= vec3(box(rotate(translate(pos, vec2(200, 200)), 3.14159/12), vec2(250, 325)), 0.0, 0.0);
 
-    FragColor = vec4(result, 1.0);
+    if (result.x >= 1.0) {
+        result.y = abs(result.x) / 100.0;
+    }
+
+    FragColor = vec4(sin(result / 1000.0), 1.0);
+    // FragColor = vec4(result, 1.0);
 }

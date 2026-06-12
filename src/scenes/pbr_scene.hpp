933c052dcae184da8c35c76c559c03505685fec7
Borin Ouch <borin.ouch@gmail.com>
2026-06-11 22:29:28 -0700

    Remove commented out lines

--- src/scenes/pbr_scene.hpp
+++ src/scenes/pbr_scene.hpp
@@ -36,131 +36,12 @@ public:
     ctx_->SetBackgroundColor(bg_color_);
 
     shader_ = Shader::FromFiles("resources/shaders/pbr_scene/main.vs", "resources/shaders/pbr_scene/main.fs");
-    // ssao_shader_ = Shader::FromFiles("resources/shaders/pbr_scene/ssao.vs", "resources/shaders/pbr_scene/ssao.fs");
-    // blur_shader_ = Shader::FromFiles("resources/shaders/pbr_scene/blur.vs", "resources/shaders/pbr_scene/blur.fs");
-    // lighting_shader_ = Shader::FromFiles("resources/shaders/pbr_scene/main.vs", "resources/shaders/pbr_scene/main.fs");
-    // light_shader_ = Shader::FromFiles("resources/shaders/pbr_scene/light.vs", "resources/shaders/pbr_scene/light.fs");
 
     projection_ = glm::perspective(glm::radians(camera_.fov), aspect_ratio_, 0.1f, 100.0f);
     camera_.position = glm::vec3(9.5f, 0.5f, 5.0f);
     camera_.yaw = -148.0f;
     camera_.pitch = -4.5f;
     camera_.UpdateCameraVectors();
-
-    // auto rand = Random::RandFloat();
-    // for (auto idx = 0; idx < 64; idx++) {
-    //   glm::vec3 sample(
-    //     rand.Next() * 2.0 - 1.0,
-    //     rand.Next() * 2.0 - 1.0,
-    //     rand.Next()
-    //   );
-    //   sample = glm::normalize(sample);
-    //   sample *= rand.Next();
-
-    //   float scale = static_cast<float>(idx) / 64.0f;
-    //   scale = Util::Lerp(0.1f, 1.0f, scale * scale);
-    //   sample *= scale;
-    //   ssao_kernel_.push_back(sample);
-    // }
-
-    // std::array quad_vertices{
-    //   -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
-    //   -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
-    //    1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
-    //    1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-    // };
-
-    // glGenVertexArrays(1, &quad_vao_);
-    // glGenBuffers(1, &quad_vbo_);
-    // glBindVertexArray(quad_vao_);
-    // glBindBuffer(GL_ARRAY_BUFFER, quad_vbo_);
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(quad_vertices), quad_vertices.data(), GL_STATIC_DRAW);
-    // glEnableVertexAttribArray(0);
-    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
-    // glEnableVertexAttribArray(1);
-    // glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    // glBindVertexArray(0);
-
-    // auto [frame_width, frame_height] = ctx_->GetFramebufferSize();
-
-    // glGenFramebuffers(1, &g_buffer_);
-    // glBindFramebuffer(GL_FRAMEBUFFER, g_buffer_);
-
-    // glGenTextures(1, &g_position_);
-    // glBindTexture(GL_TEXTURE_2D, g_position_);
-    // glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, frame_width, frame_height, 0, GL_RGBA, GL_FLOAT, nullptr);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    // glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, g_position_, 0);
-
-    // glGenTextures(1, &g_normal_);
-    // glBindTexture(GL_TEXTURE_2D, g_normal_);
-    // glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, frame_width, frame_height, 0, GL_RGBA, GL_FLOAT, nullptr);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    // glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, g_normal_, 0);
-
-    // glGenTextures(1, &g_albedo_spec_);
-    // glBindTexture(GL_TEXTURE_2D, g_albedo_spec_);
-    // glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, frame_width, frame_height, 0, GL_RGBA, GL_FLOAT, nullptr);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    // glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_TEXTURE_2D, g_albedo_spec_, 0);
-
-    // glGenRenderbuffers(1, &depth_rbo_);
-    // glBindRenderbuffer(GL_RENDERBUFFER, depth_rbo_);
-    // glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, frame_width, frame_width);
-    // glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depth_rbo_);
-
-    // std::array<GLenum, 3> attachments{ GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2 };
-    // glDrawBuffers(attachments.size(), attachments.data());
-
-    // if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-    //   LogWarning("Framebuffer not complete: g_buffer_");
-    // }
-
-    // glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-    // std::vector<glm::vec3> ssao_noise;
-    // for (auto idx = 0; idx < 16; idx++) {
-    //   glm::vec3 noise(
-    //     rand.Next() * 2.0 - 1.0,
-    //     rand.Next() * 2.0 - 1.0,
-    //     0.0f
-    //   );
-
-    //   ssao_noise.push_back(noise);
-    // }
-
-    // glGenTextures(1, &noise_texture_);
-    // glBindTexture(GL_TEXTURE_2D, noise_texture_);
-    // glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, 4, 4, 0, GL_RGB, GL_FLOAT, ssao_noise.data());
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    // glGenFramebuffers(1, &ssao_fbo_);
-    // glBindFramebuffer(GL_FRAMEBUFFER, ssao_fbo_);
-    // glGenTextures(1, &ssao_color_buffer_);
-    // glBindTexture(GL_TEXTURE_2D, ssao_color_buffer_);
-    // glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, frame_width, frame_height, 0, GL_RED, GL_FLOAT, nullptr);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    // glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, ssao_color_buffer_, 0);
-    // glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-    // glGenFramebuffers(1, &ssao_blur_fbo_);
-    // glBindFramebuffer(GL_FRAMEBUFFER, ssao_blur_fbo_);
-    // glGenTextures(1, &ssao_blur_color_buffer_);
-    // glBindTexture(GL_TEXTURE_2D, ssao_blur_color_buffer_);
-    // glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, frame_width, frame_height, 0, GL_RED, GL_FLOAT, nullptr);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    // glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, ssao_blur_color_buffer_, 0);
-    // glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
     InitSphere();
 
@@ -311,7 +192,6 @@ public:
         RenderSphere();
       }
     }
-
   }
 
   void RenderInterface(int window_width, int window_height) override {
